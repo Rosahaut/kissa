012d5f50b41aa5f6a9dadd9f3e6659a3c9acbf79
John Smith <maili@example.com>
2024-01-17 20:44:00 +0200

    jeejee

---
+++ moduuli1/tehtävä 2 kohta 6.py
@@ -0,0 +1,15 @@
+#Kirjoita ohjelma, joka arpoo ja tulostaa kaksi erilaista numerolukon koodia:
+#kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
+#nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
+
+import random
+x = random.randint(0,9)
+y = random.randint(0,9)
+z = random.randint(0,9)
+print(x,y,z)
+
+a = random.randint(1,6)
+b = random.randint(1,6)
+c = random.randint(1,6)
+d = random.randint(1,6)
+print(a,b,c,d)
