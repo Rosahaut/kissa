a1f710580bdbb0130a6490767e8d297778d96550
John Smith <maili@example.com>
2024-01-21 22:17:50 +0200

    jeejee

---
+++ moduuli1/tehtävä4_kohta1.py
@@ -0,0 +1,10 @@
+def kolmella_jaolliset():
+    luku = 1
+
+    while luku <= 1000:
+        if luku % 3 == 0:
+            print(luku)
+        luku += 1
+
+if __name__ == "__main__":
+    kolmella_jaolliset()
