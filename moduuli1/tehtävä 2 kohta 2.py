319aa5692f68d95be1bf9d40b3ced28e3c95f385
John Smith <maili@example.com>
2024-01-21 19:55:00 +0200

    jeejee

--- moduuli1/tehtävä 2 kohta 2.py
+++ moduuli1/tehtävä 2 kohta 2.py
@@ -1,5 +1,6 @@
 #Kirjoita ohjelma, joka kysyy ympyrän säteen ja tulostaa sen pinta-alan.
-PII = 3.14
-Sade = int(input("anna ympyrän säde"))
-Pinta_ala = PII * Sade** 2
+
+import math
+Sade = float(input("anna ympyrän säde"))
+Pinta_ala = math.pi * Sade** 2
 print("ympyrän pinta_ala on " + str(Pinta_ala))
