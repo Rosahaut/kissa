4e72ccb42eca29a35f44dc64fa33f40f40fa0346
John Smith <maili@example.com>
2024-01-25 12:30:49 +0200

    jeejee

--- moduuli3/tehtävä3_kohta1.py
+++ moduuli3/tehtävä3_kohta1.py
@@ -2,21 +2,16 @@
 #Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle, montako senttiä alimmasta sallitusta pyyntimitasta puuttuu.
 #Kuha on alamittainen, jos sen pituus on alle 37 cm.
 
-def tarkista_kuha_pituus():
-    alaraja = 37
+kuha_pituus = float(input("Anna kuhan pituus senttimetreinä: "))
 
-    try:
-        kuhan_pituus = float(input("Syötä kuhan pituus senttimetreinä: "))
-        if kuhan_pituus < alaraja:
-            puuttuvat_sentit = alaraja - kuhan_pituus
-            print(f"Kuha on alamittainen. Laske se takaisin järveen. Puuttuvat sentit: {puuttuvat_sentit} cm.")
-        else:
-            print("Kuha on sallitun mittainen. Voit pitää sen.")
-    except ValueError:
-        print("Virheellinen syöte. Syötä kuhan pituus numeroina.")
+alamitta_raja = 37
 
-if __name__ == "__main__":
-    tarkista_kuha_pituus()
+if kuha_pituus < alamitta_raja:
+    alimman_mitan_puute = alamitta_raja - kuha_pituus
+    print(f"Kuha on alamittainen! Laske se takaisin järveen. Puuttuu {alimman_mitan_puute} cm alimmasta sallitusta pyyntimitasta.")
+else:
+    print("Kuha on sallitun mittainen. saat pitää!.")
+
 
 
 
