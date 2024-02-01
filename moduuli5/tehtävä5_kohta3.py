ee1ad2fc672aa1f7080330ef16afd17420f9e2bc
John Smith <maili@example.com>
2024-02-01 22:52:23 +0200

    jeejee

--- moduuli5/tehtävä5_kohta3.py
+++ moduuli5/tehtävä5_kohta3.py
@@ -5,3 +5,24 @@
 # että jako menee tasan.
 #*Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla
 # 3 tai luvulla 7.
+
+def prime_number(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Kysytään käyttäjältä kokonaisluku
+user_number = int(input("enter an integer: "))
+
+# Tarkistetaan, onko annettu luku alkuluku vai ei
+prime_number_status = prime_number(user_number)
+
+# Tulostetaan tulos
+if prime_number_status:
+    print(f"{user_number} is prime number. ")
+else:
+    print(f"{user_number} is not prime number. ")
+
