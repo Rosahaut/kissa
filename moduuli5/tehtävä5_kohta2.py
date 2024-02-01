edbe82399208693822057d231349556bc7a610b0
John Smith <maili@example.com>
2024-02-01 22:35:12 +0200

    jeejee

--- moduuli5/tehtävä5_kohta2.py
+++ moduuli5/tehtävä5_kohta2.py
@@ -3,3 +3,23 @@
 #saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
 #Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille
 #argumentiksi reverse=True.
+
+numbers = []
+
+while True:
+    user_entry = input("give a number (empty string to finish): ")
+
+    if not user_entry:
+         break
+    try:
+        number = int(user_entry)
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input. Enter an integer. ")
+
+sorted_numbers = sorted(numbers, reverse=True)
+
+print("Five largest numbers in ascending order: ")
+print(sorted_numbers[:5])
+
+
