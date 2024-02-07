bd22fe9705b8db80759781da95534c7d9d8f9386
John Smith <maili@example.com>
2024-02-07 21:07:09 +0200

    jeejee

--- moduuli7/tehtävä7_kohta2.py
+++ moduuli7/tehtävä7_kohta2.py
@@ -3,3 +3,24 @@
 #tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan, syötettiinkö nimi ensimmäistä
 #kertaa. Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa
 #järjestyksessä. Käytä joukkotietorakennetta nimien tallentamiseen.
+
+names = []
+
+while True:
+    name = input("Give name: ")
+    if not name:
+        break
+    if name in names:
+        print("Same name")
+    else:
+        print("New name")
+        names.append(name)
+
+print(names)
+
+
+
+
+
+
+
