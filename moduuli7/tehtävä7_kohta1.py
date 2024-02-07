23a342e08a02c298d9ac5ac5b7f699638d4e44b3
John Smith <maili@example.com>
2024-02-07 10:48:48 +0200

    jeejee

---
+++ moduuli7/tehtävä7_kohta1.py
@@ -0,0 +1,11 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron, jonka jälkeen ohjelma
+#tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talvi). Tallenna ohjelmassasi
+#kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen. Määritellään kukin
+#vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
+
+season = ("winter", "winter", "spring", "spring", "spring", "summer",
+          "summer", "summer", "autumn", "autumn", "autumn", "winter")
+order = int(input("Give season order (1-12): "))
+month = season[order -1]
+
+print(f"{order}. month is {month}.")
