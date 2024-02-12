bf76a4efeb2ed7cd4e57b8cb70db878a42205e0e
John Smith <maili@example.com>
2024-02-12 22:09:24 +0200

    jeejee

--- moduuli8/tehtävä8_kohta2.py
+++ moduuli8/tehtävä8_kohta2.py
@@ -12,9 +12,10 @@ connection = mysql.connector.connect(
     user="root",
     password="root",
     autocommit=True
+)
 
-def find_country_by_code(iso_code):
-    sql = f"SELECT name, continent FROM country WHERE iso_country= '{iso_code}'"
+def count_airports_by_type(iso_code):
+    sql = f"SELECT type FROM airport WHERE iso_country= '{iso_code} ORDER by type'"
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchone()
@@ -22,34 +23,8 @@ def find_country_by_code(iso_code):
         return result
 
 
-user_input = input("anna maakoodi: ")
-country = find_country_by_code(user_input)
-#jos country != None
-if country:
-    print(f"Löydetty maa: {country[0]}, {country[1]}")
-else:
-    print("ei tuloksia.")
+#Kesken, en ole saanut ratkaistua
 
 
-    def uptade_country_name_by_code(iso_code, name):
-        sql = f"UPDATE country SET name='{name}' WHERE iso_country= '{iso_code}'"
-        #tarkasta että sql on oikein muodostettu
-        #print(sql)
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        if cursor.rowcount == 1:
-            print(f"tietue päivitetty ({iso_code}: {name}).")
-            return True
-        print("pieleen meni.")
-
-code_input = input("anna maakoodi: ")
-country_input = input("uusi nimi:")
-uptade_country_name_by_code(code_input, country_input)
 
 
-country = find_country_by_code(code_input)
-#jos country != None
-if country:
-    print(f"Löydetty maa: {country[0]}, {country[1]}")
-else:
-    print("ei tuloksia.")
